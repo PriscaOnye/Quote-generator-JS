40e741cb5d35bfc976831e90a3b2e91672614c03
Prisca <70857042+PriscaOnyee@users.noreply.github.com>
2023-08-02 18:31:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const quoteButton = document.getElementById("new-quote");
+const LinkedIn_Share = document.getElementById("share");
+const api_url = "https://api.quotable.io/random";
+
+async function getquote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    // console.log(data);
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+
+
+quoteButton.addEventListener("click", function() {
+    getquote(api_url)
+});
+
+
+getquote(api_url);
+
+LinkedIn_Share.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/share?text=" + quote.innerHTML + "%0A%0A __ by  " + author.innerHTML, "LinkedIn window", "width=600, height=500");
+});
 
